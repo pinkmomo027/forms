3a429b4b2b1d957b7e372261d0411ca096a7dc95
Xiangning Li <xl628h@yp.com>
2015-07-01 16:20:22 -0700

    Browser function fix

--- refactor.js
+++ refactor.js
@@ -9,54 +9,35 @@ var YpEventTracker = (function (yet) {
 		btn.addEventListener('click', track, false);
 	}	
 
+	function parseQuery ( query ) {
+	   var Params = new Object ();
+	   if ( ! query ) return Params; // return empty object
+	   var Pairs = query.split(/[;&]/);
+	   for ( var i = 0; i < Pairs.length; i++ ) {
+	      var KeyVal = Pairs[i].split('=');
+	      if ( ! KeyVal || KeyVal.length != 2 ) continue;
+	      var key = unescape( KeyVal[0] );
+	      var val = unescape( KeyVal[1] );
+	      val = val.replace(/\+/g, ' ');
+	      Params[key] = val;
+	   }
+	   return Params;
+	}
+
+
 	yet.init = function () {
 		setupListeners();
 	}
-
 
 	// extra query params from this JS file
 	yet.parseParams = function () {
 		var scripts = document.getElementsByTagName('script');
 		var script  = scripts[scripts.length - 1];
 		var queryString = script.src.replace(/^[^\?]+\??/,'');
-		function parseQuery ( query ) {
-		   var Params = new Object ();
-		   if ( ! query ) return Params; // return empty object
-		   var Pairs = query.split(/[;&]/);
-		   for ( var i = 0; i < Pairs.length; i++ ) {
-		      var KeyVal = Pairs[i].split('=');
-		      if ( ! KeyVal || KeyVal.length != 2 ) continue;
-		      var key = unescape( KeyVal[0] );
-		      var val = unescape( KeyVal[1] );
-		      val = val.replace(/\+/g, ' ');
-		      Params[key] = val;
-		   }
-
+		
 		return params = parseQuery(queryString);
 	}	
 
-	// console.log( 'illwer' );
-	// console.log( yet.parseParams("refactor.js?fn=contact_form&furl=/forms/post.php")) ;
-
-	//  to send XMLRequest
-	yet.send = function (data) {
-    	var xmlhttp;
-    	if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-		    xmlhttp=new XMLHttpRequest();
-		} else {// code for IE6, IE5
-			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-		}
-		xmlhttp.onreadystatechange = function() {
-		    if (xmlhttp.readyState==4 && xmlhttp.status==200) 
-		    	console.log("Hello It worked");
-		        // document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
-		    }
-		}
-
-		xmlhttp.open('POST', 'post.html', true);
-		xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-		xmlhttp.send("name=liz");
-	}
 	// to get All form fields 
 	yet.get = function () {
 		var inputs, index;
@@ -64,17 +45,22 @@ var YpEventTracker = (function (yet) {
 		for (index = 0; index < inputs.length; ++index) {
 		    // deal with inputs[index] element.
 		}
-
 	}
 
 	return yet;
 
 })(YpEventTracker || {});
 
+
+// console.log( 'illwer' );
+// console.log( yet.parseParams("refactor.js?fn=contact_form&furl=/forms/post.php")) ;
+	
+
+
 function run () {
 	 // Non-Microsoft browsers
 	if ( document.addEventListener ) {
-		document.addEventListener("DOMContentLoaded", YpEventTracker.init, false);
+		document.addEventListener("DOMContentLoaded", YpEventTracker.init, false)
         document.removeEventListener( "DOMContentLoaded", arguments.callee, false );
 
 	 // Microsoft
