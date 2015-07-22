b06363b0c9d81e721ff595cfd69c81d63065b222
Xiangning Li <xl628h@yp.com>
2015-07-22 15:55:34 -0700

    udpate form capture

--- refactor.js
+++ refactor.js
@@ -1,7 +1,9 @@
 var YpEventTracker = (function (yet) {
 
+	// Reference jQuery
 	var ypSubmitterTypes = /^(?:submit|button|image|reset|file)$/i,
-		ypSubmittable = /^(?:input|select|textarea|keygen)/i;
+		ypSubmittable = /^(?:input|select|textarea|keygen)/i,
+		ypCheckableType = (/^(?:checkbox|radio)$/i);
 
 	// fn: FormName
 	// fid: FormID
@@ -9,21 +11,11 @@ var YpEventTracker = (function (yet) {
 	// fb: FormSubmitButton
 
 	
-	// Request image pixel
-	function track() {
-		var url = "http://127.0.0.1:8080/rp-dataserver/ws/rpConfig";
-		var img = document.createElement('img');
-		img.setAttribute('src', url);
-		img.setAttribute('alt', 'tracking pixel');
-		img.setAttribute('height', '1');
-		img.setAttribute('width', '1');
-		img.className = "trackingImg";
-		console.log("TRACK");
-	}
+	
 
 	function setupListeners () {
 		var btn= document.getElementById('ws');
-		btn.addEventListener('click', track, false);
+		btn.addEventListener('click', yet.track, false);
 	}	
 
 	function parseQuery ( query ) {
@@ -42,18 +34,26 @@ var YpEventTracker = (function (yet) {
 	}
 
 	function serialize (FormName) {
-		var params;
-		for(i=0; i<document.FormName.elements.length; i++)
+		var params = '';
+		var tmp = [];
+		for(i=0; i<document[FormName].elements.length; i++)
 		{
-		   var fieldName = document.FormName.elements[i].name;
-		   var fieldValue = document.FormName.elements[i].value;
-		   params += fieldName + '=' + fieldValue + '&';
+		   var fieldName = document[FormName].elements[i].name;
+		   var fieldValue = document[FormName].elements[i].value;
+		   var elem = document[FormName].elements[i];
+		   // if (ypSubmittable.test( elem.tagName ) && !ypSubmitterTypes.test( elem.type ) &&
+		   // ( ) )
+
+			if (ypSubmittable.test( elem.tagName ) && !ypSubmitterTypes.test( elem.type ))
+				params += fieldName + '=' + fieldValue + '&';
 		}
 		return params;
 	}
 
 
 	yet.init = function () {
+		yet.queryString = '';
+
 		var params = yet.parseParams();
 		for (selector in params) {
 			yet.selectForms(selector, params[selector])
@@ -68,18 +68,10 @@ var YpEventTracker = (function (yet) {
 		var scripts = document.getElementsByTagName('script');
 		var script  = scripts[scripts.length - 1];
 		var queryString = script.src.replace(/^[^\?]+\??/,'');
+		yet.queryString = queryString;
 		
 		return params = parseQuery(queryString);
 	}	
-
-	// to get All form fields 
-	yet.get = function () {
-		var inputs, index;
-		inputs = document.getElementsByTagName('input');
-		for (index = 0; index < inputs.length; ++index) {
-		    // deal with inputs[index] element.
-		}
-	}
 
 	yet.selectForms = function (selector, value) {
 		console.log(selector, value);
@@ -95,12 +87,12 @@ var YpEventTracker = (function (yet) {
 			var form = document.getElementById(value);
 
 		// Select Form by URL
-		if (selector == 'fu')
+		if (selector == 'furl')
 			var form = document.querySelectorAll("[action='" + value + "'']")[0];
 
 		// Select Form by button
 		if (selector == 'fb') {
-			var btn = document.getElementById(value);
+			var btn = document.getElementsByName(value)[0];
 			var parent = btn.parentElement;
 			while(parent != null) {
 				if (parent.tagName == 'FORM')
@@ -108,13 +100,37 @@ var YpEventTracker = (function (yet) {
 				else
 					parent = parent.parentElement;
 			}
-			console.log(parent);
+			// console.log(parent);
 		}
 		
 		
 		if (form != undefined && form.method == "post")
-			form.addEventListener('submit', track, false);
+			form.addEventListener('submit', yet.track, false);
 
+		// if NO valid forms found
+		// if (form == undefined) {
+		// }
+
+	}
+
+	// Request image pixel
+	yet.track = function() {
+		var params = serialize('myForm') 
+					+ yet.queryString
+					+ "&FORM_URL=" + window.location.host
+					+ "&aid=1472960";
+		// var url = "http://127.0.0.1:8080/rp-dataserver/ws/rpConfig?" + params;
+		var url = "http://127.0.0.1:8080/rp-dataserver/ws/rpFormFill?" + params;
+		var img = document.createElement('img');
+		img.setAttribute('src', url);
+		img.setAttribute('alt', 'tracking pixel');
+		img.setAttribute('height', '1');
+		img.setAttribute('width', '1');
+		img.className = "trackingImg";
+		console.log("TRACK", url);
+		
+		// Test URL ( to insert a form field : name )
+		// var url = 'http://127.0.0.1:8080/rp-dataserver/ws/rpFormFill?aId=102220&fn=fn&fu=fu&kw=kw&name=liz'
 	}
 
 	return yet;
